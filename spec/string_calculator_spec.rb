c308b1c957dc1297df4034a78a387f0e72beabf2
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-02-03 16:29:49 +0530

    Added test cases for delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,21 @@ RSpec.describe StringCalculator do
 			expect(StringCalculator.add("1, 3, 4")).to eq(8)
 		end
 
+		it "should add numbers between new lines" do
+			expect(StringCalculator.add("1\n2,3")).to eq(6)
+		end
+
+		it 'should handle space between numbers' do
+			expect(StringCalculator.add("1 2 3")).to eq(6)
+		end
+
+		it 'should not return wrong value' do
+			expect(StringCalculator.add(1, 3, 4)).not_to eq(10)
+		end
+
+		it 'should support different delimiters' do
+			expect(StringCalculator.add("//;\n1;2")).to eq(3)
+		end
+
 	end
 end
