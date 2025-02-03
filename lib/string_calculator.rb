7e9e6bf2edba13c1e8f7e38cbf3f143f785f11c8
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-02-03 16:35:58 +0530

    Updated code for empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class StringCalculator
+
+	def self.add(numbers)
+
+		#Return 0 if number is empty or nil
+		return 0 if numbers.empty? || numbers.nil?
+		return numbers if numbers.is_a?(Integer)
+
+		#Find delimeters
+	end
 
 end
