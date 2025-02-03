2668dbe6c7cf43f55c8ef7eb48fd002755b40300
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-02-03 16:44:13 +0530

    Updated code for delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,6 +8,23 @@ class StringCalculator
 		return numbers if numbers.is_a?(Integer)
 
 		#Find delimeters
+		delimeter, numbers_list = get_delimeter(numbers)
+
+		#Sum of numbers
+		numbers.split(',').map(&:to_i).sum 
+	end
+
+	private
+
+	def self.get_delimeter(numbers)
+		if numbers.start_with?('//')
+		   delimiter, numbers = numbers.split("\n", 2)
+           delimiter = delimiter[2..-1]
+           numbers = numbers.gsub(delimiter, ",")
+        else
+           delimiter = /,|\n|\s|;/
+        end
+        return delimiter, numbers
 	end
 
 end
